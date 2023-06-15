b19fd6b19009989cf8122522736df03e9e6821d3
Felix Aristio <felixaristio@gmail.com>
2023-06-15 22:37:01 +0700

    fix UAS

---
+++ main.js
@@ -0,0 +1,35 @@
+function textup(){   
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("up");
+    document.getElementById("result").innerHTML = "UP";
+}
+
+function textleft(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("left");
+    document.getElementById("result").innerHTML = "LEFT";
+}
+
+function textright(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("right");
+    document.getElementById("result").innerHTML = "RIGHT";
+}
+
+function textdown(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("down");
+    document.getElementById("result").innerHTML = "DOWN";
+}
+
+function texta(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("a");
+    document.getElementById("result").innerHTML = "A";
+}
+
+function textb(){
+    var text = document.getElementById("result");
+    var btn1 = document.getElementById("b");
+    document.getElementById("result").innerHTML = "B";
+}
