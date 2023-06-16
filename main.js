e79b9c3bed81720d63a2b4f90f088b21e6e94a29
Felix Aristio <felixaristio@gmail.com>
2023-06-16 18:31:39 +0700

    fix javascript

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
 function textup(){   
-    var text = document.getElementById("result");
-    var btn1 = document.getElementById("up");
-    document.getElementById("result").innerHTML = "UP";
+    var text = document.getElementById("result").innerHTML = "UP";
 }
 
 function textleft(){
-    var text = document.getElementById("result");
-    var btn1 = document.getElementById("left");
-    document.getElementById("result").innerHTML = "LEFT";
+    var text = document.getElementById("result").innerHTML = "LEFT";
 }
 
 function textright(){
-    var text = document.getElementById("result");
-    var btn1 = document.getElementById("right");
-    document.getElementById("result").innerHTML = "RIGHT";
+    var text = document.getElementById("result").innerHTML = "RIGHT";
 }
 
 function textdown(){
-    var text = document.getElementById("result");
-    var btn1 = document.getElementById("down");
-    document.getElementById("result").innerHTML = "DOWN";
+    var text = document.getElementById("result").innerHTML = "DOWN";
 }
 
 function texta(){
-    var text = document.getElementById("result");
-    var btn1 = document.getElementById("a");
-    document.getElementById("result").innerHTML = "A";
+    var text = document.getElementById("result").innerHTML = "A";
 }
 
 function textb(){
-    var text = document.getElementById("result");
-    var btn1 = document.getElementById("b");
-    document.getElementById("result").innerHTML = "B";
+    var text = ocument.getElementById("result").innerHTML = "B";
 }
